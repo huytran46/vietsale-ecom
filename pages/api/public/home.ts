c40d19bc187e0d6d2e4e21273e590fc299cc9b76
tranqhuy46 <tranquochuy4698@gmail.com>
2021-10-23 01:26:56 +0700

    add logs

--- pages/api/public/home.ts
+++ pages/api/public/home.ts
@@ -4,13 +4,17 @@ import fetcher from "services/config";
 import withSession from "utils/session";
 
 export default withSession(async (req, res) => {
-  const result = await fetcher.get<BaseReponse<HomeInfo>>("/public/home");
+  try {
+    const result = await fetcher.get<BaseReponse<HomeInfo>>("/public/home");
+    if (result.status !== 200) {
+      return res.status(result.status).json({});
+    }
 
-  if (result.status !== 200) {
-    return res.status(result.status).json({});
+    const serverRes = result.data;
+    if (serverRes.data) return res.json(serverRes.data);
+    return res.status(404).json({ error: "Not found" });
+  } catch (error) {
+    console.log("[public/home] error:", error);
+    res.status(500).json({ error: "Internal server error" });
   }
-
-  const serverRes = result.data;
-  if (serverRes.data) return res.json(serverRes.data);
-  return res.status(404).json({ error: "Not found" });
 });
