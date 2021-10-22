6365c9006cb338228273593ca106c9613a5e46f2
tranqhuy46 <tranquochuy4698@gmail.com>
2021-10-23 01:44:49 +0700

    add more logs

--- pages/api/public/home.ts
+++ pages/api/public/home.ts
@@ -6,12 +6,13 @@ import withSession from "utils/session";
 export default withSession(async (req, res) => {
   try {
     const result = await fetcher.get<BaseReponse<HomeInfo>>("/public/home");
-    if (result.status !== 200) {
+    console.log("[public/home] result:", result);
+    if (result?.status !== 200) {
       return res.status(result.status).json({});
     }
 
-    const serverRes = result.data;
-    if (serverRes.data) return res.json(serverRes.data);
+    const serverRes = result?.data;
+    if (serverRes?.data) return res.json(serverRes.data);
     return res.status(404).json({ error: "Not found" });
   } catch (error) {
     console.log("[public/home] error:", error);
