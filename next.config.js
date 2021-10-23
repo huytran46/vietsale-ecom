11021c3a85d0d4333e3e644fe83e23ddd2c2a72d
tranqhuy46 <tranquochuy4698@gmail.com>
2021-10-23 12:45:11 +0700

    update nextjs config

--- next.config.js
+++ next.config.js
@@ -1,5 +1,13 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
+  async rewrites() {
+    return [
+      {
+        source: "/external/:path*",
+        destination: "http://157.245.61.181:8081/api/v1/:path*",
+      },
+    ];
+  },
   reactStrictMode: true,
   images: {
     domains: ["fakeimg.pl"],
