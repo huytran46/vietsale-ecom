f7e153734620a0ff0a4dbef7498a65f9c5f9eb3f
tranqhuy46 <tranquochuy4698@gmail.com>
2021-10-24 11:27:44 +0700

    revert next config

--- next.config.js
+++ next.config.js
@@ -1,17 +1,5 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
-  async rewrites() {
-    return [
-      {
-        source: "/_dev/:path*",
-        destination: "http://157.245.61.181:8081/api/v1/:path*",
-      },
-      {
-        source: "/_prod/:path*",
-        destination: "http://157.245.156.225:8081/api/v1/:path*",
-      },
-    ];
-  },
   reactStrictMode: true,
   images: {
     domains: ["fakeimg.pl"],
