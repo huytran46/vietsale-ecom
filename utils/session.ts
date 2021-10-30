165a4fa81914ce1b38685063b0ae043cff8123ba
tranqhuy46 <tranquochuy4698@gmail.com>
2021-10-30 17:13:09 +0700

    remove logs

--- utils/session.ts
+++ utils/session.ts
@@ -47,7 +47,6 @@ export function withAuth(
 ): NextIronHandler {
   return async (req, res) => {
     const token = req.session.get(IronSessionKey.AUTH);
-    console.log("req.session in withAuth:", token);
     if (!token) {
       req.session.destroy();
       await req.session.save();
