b95adb1124000558b4d21cc0842c1b9288d38bf0
tranqhuy46 <tranquochuy4698@gmail.com>
2022-01-08 01:38:49 +0700

    chore(lint): Empty add spacing

--- components/common/Empty.tsx
+++ components/common/Empty.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { Center, VStack, Text, Icon } from "@chakra-ui/react";
 import { BsQuestionCircle } from "react-icons/bs";
+
 const Empty: React.FC = ({ children }) => {
   return (
     <Center bg="white" mt={16} p={10} h="full" w="full">
